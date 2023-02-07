e7bbead4cdb5128fa4c27e2c35af9a020b418721
yeooo <71688432+yeoooo@users.noreply.github.com>
2023-02-07 16:17:23 +0900

    Initial commit

---
+++ backend/src/main/java/com/worldassetmap/wam/WamApplication.java
@@ -0,0 +1,13 @@
+package com.worldassetmap.wam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WamApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/worldassetmap/wam/WamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.worldassetmap.wam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
